7092bfa10aeab39c68ea7509323015323c259276
KarinaKKarinaK <karinakalicka@gmail.com>
2025-09-03 16:05:40 +0200

    Added Stack class in ds

--- algos-py/data_structures.py
+++ algos-py/data_structures.py
@@ -1,1 +1,31 @@
 # Stack, Queue, Deque (LIFO/FIFO/LILO demos)
+
+# STACKS ============================================================================================================
+# Push: Adds a new element on the stack.
+# Pop: Removes and returns the top element from the stack.
+# Peek: Returns the top (last) element on the stack.
+# isEmpty: Checks if the stack is empty.
+# Size: Finds the number of elements in the stack.
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def is_Empty(self):
+        return len(self.stack) == 0
+    
+    def push(self, element):
+        self.stack.append(element)
+
+    def pop(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.stack.pop()
+    
+    def peek(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.stack[-1]
+    
+    def size(self):
+        return len(self.stack)
