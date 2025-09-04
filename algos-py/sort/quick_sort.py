22a7acf5ff53059e837e09be63da95a512a64bc9
KarinaKKarinaK <karinakalicka@gmail.com>
2025-09-04 17:21:47 +0200

    Started quick sort

---
+++ algos-py/sort/quick_sort.py
@@ -0,0 +1,11 @@
+# QUICK SORT
+import time
+# How it works:
+
+# 1. Choose a value in the array to be the pivot element.
+# 2. rder the rest of the array so that lower values than the pivot element are on the left, and higher values are on the right.
+# 3. Swap the pivot element with the first element of the higher values so that the pivot element lands in between the lower and higher values.
+# 4. Do the same operations (recursively) for the sub-arrays on the left and right side of the pivot element.
+
+def quick_sort(list):
+    pass
