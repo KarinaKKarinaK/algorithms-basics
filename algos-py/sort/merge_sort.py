13b3c97c71660657ff425c8350a52ffc6ce86f71
KarinaKKarinaK <karinakalicka@gmail.com>
2025-09-05 12:40:09 +0200

    initializing merge sort

--- algos-py/sort/merge_sort.py
+++ algos-py/sort/merge_sort.py
@@ -1,1 +1,15 @@
 # Merge Sort
+
+# The Merge Sort algorithm is a divide-and-conquer algorithm that sorts an array by first 
+# breaking it down into smaller arrays, and then building the array back together the 
+# correct way so that it is sorted.
+
+# How it works:
+
+# 1. Divide the unsorted array into two sub-arrays, half the size of the original.
+# 2. Continue to divide the sub-arrays as long as the current piece of the array has more than one element.
+# 3. Merge two sub-arrays together by always putting the lowest value first.
+# 4. Keep merging until there are no sub-arrays left.
+
+def merge_sort(list):
+    pass
