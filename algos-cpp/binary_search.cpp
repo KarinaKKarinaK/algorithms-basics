f60bc16d3fdf7dad274427da5caeea10a19ad3bf
KarinaKKarinaK <karinakalicka@gmail.com>
2025-09-13 16:20:38 +0200

    Started programming teh binary_search function in C++

---
+++ algos-cpp/binary_search.cpp
@@ -0,0 +1,7 @@
+#include <iostream>   // For input/output
+using namespace std; // Avoids writing std:: everywhere
+
+int binary_search(const vector<int>& array, int x) {
+    int left = 0;
+    int right = array.size() - 1;
+}
