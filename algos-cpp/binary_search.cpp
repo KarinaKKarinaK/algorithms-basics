e94d75f5d5f2802f1189b63b738997b594baece9
KarinaKKarinaK <karinakalicka@gmail.com>
2025-09-13 16:23:56 +0200

    Finished binary search in C++

--- algos-cpp/binary_search.cpp
+++ algos-cpp/binary_search.cpp
@@ -4,4 +4,17 @@ using namespace std; // Avoids writing std:: everywhere
 int binary_search(const vector<int>& array, int x) {
     int left = 0;
     int right = array.size() - 1;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (array[mid] < x) {
+            left = mid + 1;
+        } else if (array[mid] > x){
+            right = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1
 }
